c346a41b1760028ed4253bd8993d30b1e865e515
Alexey Shimanskii <ShimanskiiAlexey@yandex.ru>
2024-05-29 22:39:14 +0200

    Initial Commit

---
+++ pictureList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pictureList
+//
+//  Created by Alexey Shimanskii on 29.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
